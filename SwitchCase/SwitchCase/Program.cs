abe4109246545297622fa18e03a331fa987d4b33
itlsamuel <italosamuel0@gmail.com>
2022-02-21 12:28:23 -0300

    switchCase

---
+++ SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha um mes");
+            string mes = Console.ReadLine();
+
+            mes = mes.ToLower();
+
+            switch (mes)
+            {
+                case "janeiro": case "marco": case "maio": case "julho": case "agosto":
+                case "outubro": case "desembro":
+                    Console.WriteLine("SEU MES TEM 31 DIAS");
+                    break;
+                case "abril": case "junho": case "setembro": case "novembro":
+                    Console.WriteLine("Seu mes tem 30 dias");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
