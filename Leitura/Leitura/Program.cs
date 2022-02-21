8948db3b038b30f86ac428eb5beeba1f08eff5a3
itlsamuel <italosamuel0@gmail.com>
2022-02-21 00:03:14 -0300

    second commit

---
+++ Leitura/Leitura/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leitura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 5;
+            short b = 2;
+            int result;
+
+            result = a + b;
+
+            Console.WriteLine("resultado =  {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
