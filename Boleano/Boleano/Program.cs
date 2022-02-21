f0f7782193ec546cc5f4f07d5fbaf9100a16e19b
itlsamuel <italosamuel0@gmail.com>
2022-02-21 00:22:48 -0300

    boleano"

---
+++ Boleano/Boleano/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boleano
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 7;
+            bool result;
+
+            result = x > 7;
+
+            Console.WriteLine("{0} eh maior que {1} ? resposta: {2}",x,y,result);
+            Console.ReadLine();
+        }
+    }
+}
