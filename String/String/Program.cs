0abc04ea765de5757c287807e79b2c66a9331271
itlsamuel <italosamuel0@gmail.com>
2022-02-21 00:35:16 -0300

    string

---
+++ String/String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "italo";
+            string s2 = "samuel";
+            string s3 = "andrade";
+
+            string nome1 = s1 + " " + s2;
+            string nome2 = string.Concat(s2, " ", s3);
+
+            Console.WriteLine(nome1);
+            Console.WriteLine(nome2);
+            Console.ReadKey();
+        }
+    }
+}
