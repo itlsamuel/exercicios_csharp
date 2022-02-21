74da7dffdc0b0484f3e07fb708a3c3630fdd69d9
itlsamuel <italosamuel0@gmail.com>
2022-02-21 14:58:16 -0300

    array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray01 = new int[] {10, 8, 6, 4, 2};
+            /*
+                [0] - 10
+                [1] - 8
+                [2] - 6
+                [3] - 4
+                [4] - 2
+             */
+            int num;
+
+            num = intArray01.Length;
+
+            Console.WriteLine("{0},{1},{2},{3},{4} e tamanho = {5}", intArray01[0], intArray01[1], intArray01[2],
+                intArray01[3], intArray01[4], num);
+            Console.ReadKey();
+
+        }
+    }
+}
