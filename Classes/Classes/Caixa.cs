fed1472eccfe8a4ffd7a4ef0365a3d9c0471a121
itlsamuel <italosamuel0@gmail.com>
2022-02-22 11:13:28 -0300

    classes

---
+++ Classes/Classes/Caixa.cs
@@ -0,0 +1,17 @@
+﻿namespace Classes
+{
+    internal class Caixa
+    {
+        private double lado;
+        public Caixa()
+        {
+            lado = 10;
+        }
+
+        public double Volume()
+        {
+            return lado * lado * lado;
+        }
+    }
+
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Caixa cx = new Caixa();
+            double volumeCaixa;
+            volumeCaixa = cx.Volume();
+            Console.WriteLine(volumeCaixa.ToString());
+            Console.ReadKey();
+        }
+    }
+}
