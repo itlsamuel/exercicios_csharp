56d331fd7bb219c779e77d371f7503e4d2f7f463
itlsamuel <italosamuel0@gmail.com>
2022-02-22 12:09:59 -0300

    struct

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    struct veiculo
+    {
+        public string cor;
+        public string registro;
+        public ushort ano;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            veiculo fiat = new veiculo();
+            Console.WriteLine("cor: {0}, registro: {1}, ano: {2}", fiat.cor, fiat.registro, fiat.ano);
+            Console.ReadKey();
+        }
+    }
+}
