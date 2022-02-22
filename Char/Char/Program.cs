9e722938ccaa3975817fa886215ffe1278300b32
itlsamuel <italosamuel0@gmail.com>
2022-02-21 22:02:20 -0300

    char

---
+++ Char/Char/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char primeiraPalavra;
+            ConsoleKeyInfo cki;
+
+            Console.WriteLine("Escreva uma palavra");
+
+            cki = Console.ReadKey;
+
+            primeiraPalavra = cki.KeyChar;
+
+            Console.WriteLine("\n O caracter digitado eh {0}", primeiraPalavra)
+        }
+    }
+}
