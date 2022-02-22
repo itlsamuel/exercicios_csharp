5fe64fce71d39f54467aeb1dbcf8962ab0e952aa
itlsamuel <italosamuel0@gmail.com>
2022-02-22 12:22:08 -0300

    exceptions

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 0;
+
+            try
+            {
+                Console.WriteLine("{0}/{1}={2}", a, b, (a / b));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Eu peguei a exceção!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
