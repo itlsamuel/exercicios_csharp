b6dcec9bd1da889cc009502663ddb2602a7e87c3
itlsamuel <italosamuel0@gmail.com>
2022-02-22 00:48:33 -0300

    metodos

---
+++ Metodos/Metodos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    internal class Program
+    {
+        public static float velocidade(float velo, float tempo, float aceleracao)
+        {
+            return (velo + (tempo * aceleracao));
+        }
+        public static float distancia(float velo, float tempo, float aceleracao)
+        {
+            return ((float)(velo * tempo + 0.5 * aceleracao * tempo * tempo));
+        }
+
+        static void Main(string[] args)
+        {
+            float dist;
+            float v;
+
+            v = velocidade(100, 10, 25);
+            Console.WriteLine(v);
+            Console.WriteLine("\n");
+
+            dist = distancia(100, 10, 25);
+            Console.WriteLine(dist);
+
+            Console.ReadKey();
+        }
+    }
+}
